6af52728f7b4fb6652402f822f1879045f94c62c
ta.to <61908291+tato-gh@users.noreply.github.com>
2022-11-25 08:55:16 +0900

    Add mix new application

---
+++ lib/lgbm_ex_capi.ex
@@ -0,0 +1,18 @@
+defmodule LGBMExCapi do
+  @moduledoc """
+  Documentation for `LGBMExCapi`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LGBMExCapi.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/lgbm_ex_capi_test.exs
@@ -0,0 +1,8 @@
+defmodule LGBMExCapiTest do
+  use ExUnit.Case
+  doctest LGBMExCapi
+
+  test "greets the world" do
+    assert LGBMExCapi.hello() == :world
+  end
+end
